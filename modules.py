c5cf3751290f2c88a17e7ef739fb248fdf68a1a2
Luc Salvon <salvon.luc@gmail.com>
2024-03-23 01:01:24 +0100

    AJout du biais

--- modules.py
+++ modules.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from classes_abstraites import Module
@@ -10,14 +12,15 @@ class Linear(Module):
         self.input_size = input
         self.output_size = output
 
-        self._parameters = np.random.random((self.input_size, self.output_size))
-        self._gradient = np.zeros((self.input_size, self.output_size))
+        self._parameters = np.random.uniform(-math.sqrt(self.input_size), math.sqrt(self.input_size), (self.input_size + 1, self.output_size))
+        self._gradient = np.zeros((self.input_size + 1, self.output_size))
 
     def zero_grad(self):
         self._gradient.fill(0)
 
     def forward(self, X: np.ndarray):
         assert X.shape[1] == self.input_size
+        X = np.concatenate((X, np.ones((X.shape[0], 1))), axis=1)  # Ajout du biais
 
         return X @ self._parameters
 
@@ -26,11 +29,15 @@ class Linear(Module):
         assert input.shape[1] == self.input_size
         assert delta.shape == (batch_size, self.output_size)
 
+        input = np.concatenate((input, np.ones((input.shape[0], 1))), axis=1)
+
         self._gradient += input.T @ delta
 
     def backward_delta(self, input, delta):
         batch_size = input.shape[0]
         assert input.shape[1] == self.input_size
         assert delta.shape == (batch_size, self.output_size)
+
+        X = np.concatenate((input, np.ones((input.shape[0], 1))), axis=1)
 
         return delta @ self._parameters.T
