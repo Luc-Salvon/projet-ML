e11a0a59c261415d63494201dd278157e0c93e14
Luc Salvon <salvon.luc@gmail.com>
2024-03-23 13:30:17 +0100

    Partie 3

--- main.py
+++ main.py
@@ -2,57 +2,57 @@ from matplotlib import pyplot as plt
 
 import mltools
 
-from modele import Modele
+from modele import Sequentiel, sgd
 from losses import *
 from modules import *
 
 
 def test_partie1():
-    modele = Modele([Linear(2, 1)], MSELoss())
+    net = Sequentiel([Linear(2, 1)])
 
     data = mltools.gen_arti(nbex=1000, data_type=0)
     X_train, X_test, Y_train, Y_test = data[0][:800], data[0][800:], data[1][:800], data[1][800:]
 
-    modele.fit(X_train, Y_train)
+    evolution_loss = sgd(net, (X_train, Y_train), loss=MSELoss())
 
     # Plot de la loss
-    plt.plot(modele.loss_evolution)
+    plt.plot(evolution_loss)
     plt.xlabel("Nombre d'epochs")
     plt.ylabel("Loss")
     plt.title("Evolution de la loss en fonction du nombre d'epochs")
     plt.show()
 
     # Frontiere de decision
-    mltools.plot_frontiere(X_test, modele.predict)
+    mltools.plot_frontiere(X_test, net.forward)
     mltools.plot_data(X_test, Y_test)
     plt.title("Frontiere de decision")
     plt.show()
 
 
 def test_partie2():
-    modele = Modele([Linear(2, 3), TanH(), Linear(3, 1), Sigmoide()], MSELoss())
+    net = Sequentiel([Linear(2, 3), TanH(), Linear(3, 1), Sigmoide()])
 
     data = mltools.gen_arti(nbex=1000, data_type=1)
     X_train, X_test, Y_train, Y_test = data[0][:800], data[0][800:], data[1][:800], data[1][800:]
     Y_train[Y_train == -1] = 0
     Y_test[Y_test == -1] = 0
 
-    modele.fit(X_train, Y_train, nb_epochs=1000)
+    evolution_loss = sgd(net, (X_train, Y_train), loss=MSELoss(), nb_epochs=1000)
 
     # Plot de la loss
-    plt.plot(modele.loss_evolution)
+    plt.plot(evolution_loss)
     plt.xlabel("Nombre d'epochs")
     plt.ylabel("Loss")
     plt.title("Evolution de la loss en fonction du nombre d'epochs")
     plt.show()
 
     # Frontiere de decision
-    mltools.plot_frontiere(X_test, lambda x: modele.predict(x) - .5)
+    mltools.plot_frontiere(X_test, lambda x: net.forward(x) - .5)
     mltools.plot_data(X_test, Y_test)
     plt.title("Frontiere de decision")
     plt.show()
 
-    yhat = modele.predict(X_test)
+    yhat = net.forward(X_test)
     print("Accuracy:", np.mean(np.round(yhat) == Y_test))
 
 
--- modele.py
+++ modele.py
@@ -1,43 +1,68 @@
 from typing import Iterable
 
+import numpy as np
+
 import classes_abstraites
 
 
-class Modele:
-    def __init__(self, modules: Iterable[classes_abstraites.Module], loss: classes_abstraites.Loss):
+class Sequentiel:
+    def __init__(self, modules: Iterable[classes_abstraites.Module]):
         self.modules = modules  # Liste des modules utilisés dans le modèle
-        self.loss = loss  # Fonction de loss utilisée
-        self.loss_evolution = []  # Liste de la loss à chaque epoch
 
-    def fit(self, X, Y, nb_epochs=50, eps=10e-5, batch_size=5):
-        N = X.shape[0]  # Nombre d'exemples total
-
-        for i_epoch in range(nb_epochs):  # On itère sur les epochs
-            self.loss_evolution.append(self.loss.forward(Y, self.predict(X)).mean())
-
-            # Si la loss n'évolue pas significativement on s'arrête
-            if len(self.loss_evolution) > 1 and abs(self.loss_evolution[-1] - self.loss_evolution[-2]) < eps:
-                break
-
-            for i_batch in range(0, N, batch_size):  # On itère sur les batchs
-                X_batch = X[i_batch:min(i_batch + batch_size, N)]
-                Y_batch = Y[i_batch:min(i_batch + batch_size, N)]
-
-                Yhat = self.predict(X_batch)
-
-                delta = self.loss.backward(Y_batch, Yhat)
-
-                # Backpropagation
-                for i, module in enumerate(self.modules[::-1]):
-                    module.zero_grad()
-                    module.backward_update_gradient(self.inputs[-(i+2)], delta)
-                    module.update_parameters()
-                    delta = module.backward_delta(self.inputs[-(i+2)], delta)
-
-    def predict(self, X):
+    def forward(self, X):
         self.inputs = [X]
 
         for module in self.modules:
             self.inputs.append(module.forward(self.inputs[-1]))
 
         return self.inputs[-1]
+
+    def backward(self, input, delta):
+        # Backprop -> On calcule le gradient de la fin vers le début
+        for i, module in enumerate(self.modules[::-1]):
+            module.backward_update_gradient(self.inputs[-(i + 2)], delta)
+            delta = module.backward_delta(self.inputs[-(i + 2)], delta)
+
+
+class Optim:
+    def __init__(self, net: Sequentiel, loss: classes_abstraites.Loss, eps: float):
+        self.net = net
+        self.loss = loss
+        self.eps = eps
+
+    def step(self, batch_X, batch_Y):
+        Yhat = self.net.forward(batch_X)
+
+        delta = self.loss.backward(batch_Y, Yhat)
+
+        # Calcul du gradient
+        self.net.backward(batch_X, delta)
+
+        # Update des paramètres
+        for module in self.net.modules:
+            module.update_parameters(gradient_step=self.eps)
+            module.zero_grad()
+
+
+def sgd(net: Sequentiel, data: tuple[np.ndarray], loss: classes_abstraites.Loss, batch_size: int = 5, nb_epochs: int = 50, eps: float = 10e-5, step: float = 1e-3):
+    X, Y = data[:]
+    N = X.shape[0]  # Nombre d'exemples total
+
+    optim = Optim(net, loss, step)
+
+    evolution_loss = []
+
+    for i_epoch in range(nb_epochs):  # On itère sur les epochs
+        evolution_loss.append(loss.forward(Y, net.forward(X)).mean())
+
+        # Si la loss n'évolue pas significativement on s'arrête
+        if len(evolution_loss) > 1 and abs(evolution_loss[-1] - evolution_loss[-2]) < eps:
+            break
+
+        for i_batch in range(0, N, batch_size):  # On itère sur les batchs
+            batch_X = X[i_batch:min(i_batch + batch_size, N)]
+            batch_y = Y[i_batch:min(i_batch + batch_size, N)]
+
+            optim.step(batch_X, batch_y)
+
+    return evolution_loss
